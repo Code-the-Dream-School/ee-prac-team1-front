c304f2f30aef2789ec99b519dc540956f10c96d0
Tammam <tammam.wafai@gmail.com>
2023-11-08 16:40:56 -0600

    /activities/:id route CREATED

---
+++ src/pages/ActivityDetails.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from '../components';
+
+const ActivityDetails = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Activity Details:</h1>
+    </>
+  );
+};
+
+export default ActivityDetails;
