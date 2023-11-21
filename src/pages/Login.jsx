7f41d84042e2369ef1488d6ef13738f51ef4cf18
Kremifany <34751960+Kremifany@users.noreply.github.com>
2023-11-20 23:22:32 -0500

    login page using formik and theme

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,3 @@
-// import React, { useState } from 'react'
 import {
   Box,
   Button,
@@ -6,45 +5,62 @@ import {
   Typography,
   ThemeProvider,
 } from '@mui/material'
-import { useState } from 'react'
+//import { useState } from 'react'
 import { theme } from '../utils/theme'
 import Logo from '../assets/logo70.png'
+import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+
+const validationSchema = yup.object({
+  firstName: yup
+    .string('Enter first name')
+    .required('First name is required')
+    .min(2, 'First name must be at least 2 characters long')
+    .max(50, 'First name cannot be more than 50 characters long'),
+  lastName: yup
+    .string('Enter last name')
+    .required('Last name is required')
+    .min(2, 'Last name must be at least 2 characters long')
+    .max(50, 'Last name cannot be more than 50 characters long'),
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    //.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/, 'Password must contain at least one lowercase letter, one uppercase letter, and one number'),
+    .required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .required('Confirm password is required')
+    .oneOf([yup.ref('password')], 'Confirm password must match password'),
+})
+
 const Login = () => {
-  const [isSignup, setisSignup] = useState(false)
-  const [inputs, setInputs] = useState({
-    fname: '',
-    lname: '',
-    email: '',
-    password: '',
+  const navigate = useNavigate()
+
+  const formik = useFormik({
+    initialValues: {
+      lastName: '',
+      firstName: '',
+      email: '',
+      password: '',
+    },
+
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values)
+      navigate('/')
+    },
   })
-
-  //////////////////////////////
-
-  const handleChange = (e) => {
-    setInputs((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }))
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(inputs)
-
-    console.log(isSignup)
-  }
-  //handler to reset all the fields in form ON transition from login to signup
-  const resetState = () => {
-    setisSignup(!isSignup)
-    setInputs({ fname: '', lname: '', email: '', password: '' })
-  }
-  console.log(isSignup)
 
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Box sx={{ bgcolor: 'rgba(29,230,25,0.32)' }}>
-          <form onSubmit={handleSubmit}>
+        <Box sx={{ bgcolor: theme.palette.background.main }}>
+          <form onSubmit={formik.handleSubmit}>
             <Box
               display="flex"
               flexDirection={'column'}
@@ -57,99 +73,154 @@ const Login = () => {
               borderRadius={5}
             >
               <img src={Logo} alt="Player Buddy Logo" />
-              {isSignup && (
-                <>
-                  <Typography variant="h4" padding={3} textAlign="center">
-                    <strong>Login</strong>
-                  </Typography>
-                  <Typography>
-                    <strong>Hi there! Nice to see you again.</strong>
-                  </Typography>
-                </>
-              )}
-              {!isSignup && (
-                <>
-                  <Typography variant="h4" padding={3} textAlign="center">
-                    <strong>Welcome</strong>
-                  </Typography>
-                  <Typography>
-                    <strong>Lets us help you to find your buddy</strong>
-                  </Typography>
-                </>
-              )}
-              {!isSignup && (
-                <TextField
-                  sx={{ bgcolor: '#fff' }}
-                  label="First Name"
-                  size="small"
-                  onChange={handleChange}
-                  value={inputs.fname}
-                  name="fname"
-                  margin="normal"
-                  type={'text'}
-                  fullWidth
-                  placeholder="Enter your first name"
-                  variant="outlined"
-                />
-              )}
-              {!isSignup && (
-                <TextField
-                  sx={{ bgcolor: '#fff' }}
-                  label="Last Name"
-                  size="small"
-                  onChange={handleChange}
-                  variant="outlined"
-                  bgcolor="#ffff"
-                  value={inputs.lname}
-                  name="lname"
-                  margin="normal"
-                  type={'text'}
-                  fullWidth
-                  placeholder="Enter your last name"
-                />
-              )}
+
+              <Typography
+                variant="h5"
+                padding={3}
+                textAlign="center"
+                color="theme.palette.contrastText"
+              >
+                <strong>CREATE ACCOUNT</strong>
+              </Typography>
               <TextField
-                sx={{ bgcolor: '#fff' }}
-                label="E-mail"
+                sx={{
+                  bgcolor: '#fff',
+                  '& .MuiInputLabel-root.Mui-focused':
+                    theme.overrides.MuiInputLabel.root['&.Mui-focused'],
+                  '& .MuiOutlinedInput-root':
+                    theme.overrides.MuiOutlinedInput.root,
+                }}
                 size="small"
+                margin="normal"
+                type={'text'}
+                placeholder="Enter your first name"
+                variant="outlined"
                 fullWidth
-                onChange={handleChange}
-                value={inputs.email}
-                name="email"
+                id="FirstName"
+                name="firstName"
+                label="First Name"
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.firstName && Boolean(formik.errors.firstName)
+                }
+                helperText={formik.touched.firstName && formik.errors.firstName}
+              />
+              <TextField
+                sx={{
+                  bgcolor: '#fff',
+                  '& .MuiInputLabel-root.Mui-focused':
+                    theme.overrides.MuiInputLabel.root['&.Mui-focused'],
+                  '& .MuiOutlinedInput-root':
+                    theme.overrides.MuiOutlinedInput.root,
+                }}
+                size="small"
+                margin="normal"
+                type={'text'}
+                placeholder="Enter your last name"
+                variant="outlined"
+                fullWidth
+                id="LastName"
+                name="lastName"
+                label="Last Name"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.lastName && Boolean(formik.errors.lastName)
+                }
+                helperText={formik.touched.lastName && formik.errors.lastName}
+              />
+              <TextField
+                sx={{
+                  bgcolor: '#fff',
+                  '& .MuiInputLabel-root.Mui-focused':
+                    theme.overrides.MuiInputLabel.root['&.Mui-focused'],
+                  '& .MuiOutlinedInput-root':
+                    theme.overrides.MuiOutlinedInput.root,
+                }}
+                size="small"
                 margin="normal"
                 type={'text'}
                 placeholder="Enter your e-mail"
+                variant="outlined"
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
               />
               <TextField
-                sx={{ bgcolor: '#fff' }}
-                label="Password"
+                sx={{
+                  bgcolor: '#fff',
+                  '& .MuiInputLabel-root.Mui-focused':
+                    theme.overrides.MuiInputLabel.root['&.Mui-focused'],
+                  '& .MuiOutlinedInput-root':
+                    theme.overrides.MuiOutlinedInput.root,
+                }}
                 size="small"
-                fullWidth
-                onChange={handleChange}
-                value={inputs.password}
-                name="password"
                 margin="normal"
-                type={'password'}
                 placeholder="Enter your password"
+                variant="outlined"
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
               />
-
-              <Button
-                type="submit"
-                onClick={resetState}
+              <TextField
+                sx={{
+                  bgcolor: '#fff',
+                  '& .MuiInputLabel-root.Mui-focused':
+                    theme.overrides.MuiInputLabel.root['&.Mui-focused'],
+                  '& .MuiOutlinedInput-root':
+                    theme.overrides.MuiOutlinedInput.root,
+                }}
+                size="small"
+                margin="normal"
+                placeholder="Enter your password to confirm"
+                variant="outlined"
                 fullWidth
-                sx={{ marginTop: 3, borderRadius: 3 }}
+                id="confirmPassword"
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.confirmPassword &&
+                  Boolean(formik.errors.confirmPasswordassword)
+                }
+                helperText={
+                  formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword
+                }
+              />
+              <Button
+                color="primary"
                 variant="contained"
-              >
-                {!isSignup ? 'Sign in' : 'Login'}
-              </Button>
-              <Button
-                onClick={resetState}
-                sx={{ marginTop: 3, borderRadius: 3 }}
                 fullWidth
+                type="submit"
+                sx={{
+                  ...theme.commonButtonStyles,
+                  marginLeft: 2,
+                  width: 320,
+                }}
               >
-                {!isSignup
-                  ? 'Already have an account? Log in'
-                  : "Don't have an account? Sign in"}
+                Submit
               </Button>
             </Box>
           </form>
